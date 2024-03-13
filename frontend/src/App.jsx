6fbe45f55615b8811844f3c254e0ca68981d5dc3
Alvin Dsouza <alvindsouza@Alvins-MacBook-Air.local>
2024-03-13 23:03:25 +0530

    Base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,57 +11,68 @@ import Nav from "./components/Nav";
 import Complaint from "./components/Complaint";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Main />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/login",
-				element: <div>
-          <Nav />
-          <Login />
-        </div>,
-			},
-			{
-				path: "/joytest",
-				element: <div>
-          <Nav />
-          <SpeechChatbot />
-        </div>,
-			},
-			{
-				path: "/chatbot",
-				element: <div>
-          <Nav />
-          <Chatbot />
-        </div>,
-			},
-			{
-				path: "/dashboard",
-				element: (
-					<div>
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <div>
+            <Nav />
+            <Login />
+          </div>
+        ),
+      },
+      {
+        path: "/joytest",
+        element: (
+          <div>
+            <Nav />
+            <SpeechChatbot />
+          </div>
+        ),
+      },
+      {
+        path: "/chatbot",
+        element: (
+          <div>
+            <Nav />
+            <Chatbot />
+          </div>
+        ),
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <div>
             <Nav />
             <div className="bg-gray-100 ">
-						<Dashboard />
-					</div>
+              <Dashboard />
+            </div>
           </div>
-				),
-			},
+        ),
+      },
       {
         path: "/complaints",
+        element: (
+          <div className="md:min-h-[100vh] min-h-full bg-gray-100 ">
+            <Nav />
+            <ComplaintList />
+          </div>
+        ),
+      },
+      {
         element: (
           <div className="md:min-h-[100vh] min-h-full bg-gray-100 ">
             <Nav />
             <Complaint />
           </div>
         ),
-      },
-      {
-        element: <Complaint />,
         path: "complaints/:complaintId",
       },
     ],
