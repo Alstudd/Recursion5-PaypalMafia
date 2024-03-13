ec84fc92eafa2974e6cbdfb83bf2139ad0b51f97
Joyvin <joymendonca710@gmail.com>
2024-03-13 21:54:07 +0530

    lex

--- uagents/main.py
+++ uagents/main.py
@@ -7,6 +7,23 @@ access_key = "AKIAQ6DRLSHDL5G3EC42"
 access_secret = "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"
 region = "us-east-1"
 
-client = boto3.client('lexv2-models', region_name=region,
+client = boto3.client('lexv2-runtime', region_name=region,
                       aws_access_key_id=access_key,
                       aws_secret_access_key=access_secret)
+
+bot_id = 'SN0WUY4X6A'
+bot_alias_id = 'TSTALIASID'
+locale_id = 'en_US'
+
+
+user_input = 'What is the weather today?'
+session = 'test'
+
+response = client.recognize_utterance(
+    botId=bot_id,
+    botAliasId=bot_alias_id,
+    localeId=locale_id,
+    sessionId=session,
+    inputStream=user_input,
+    requestContentType="text/plain"
+)
