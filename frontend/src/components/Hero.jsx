7981939ca4d57fa13fe1038ff6b06b9625391d3e
Alstudd <alstonsoares17@gmail.com>
2024-03-20 00:37:38 +0530

    hero changes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/globals.css";
+import { Home, Bot, LayoutDashboard, Rocket } from "lucide-react"
 
 const Hero = () => {
   return (
     <div className="w-full -mt-24">
-      <div className="flex flex-col items-center justify-center h-[850px] bgimg px-4 mb-16 max-sm:mt-4">
+      <div className="flex flex-col items-center justify-center h-[860px] bgimg px-4 mb-16 max-sm:mt-4">
         <div className="mx-auto px-4 py-1 bg-white bg-opacity-20 rounded-full text-md text-white mb-2 backdrop-blur-sm cursor-pointer max-sm:text-sm">
           <a href="#">Public Square is working on a revloution 🚀👨‍💻</a>
         </div>
@@ -21,30 +22,37 @@ const Hero = () => {
             </span>
           </p>
         </div>
-        <a href="/user-dashboard">
+        
           <div className="p-2 w-fit bg-black gap-3 backdrop-blur-md flex rounded-full z-10 relative top-7 bg-opacity-30">
+          <a href="/">
             <div className="bg-white h-14 rounded-full py-2 pl-4 px-5 w-fit flex items-center justify-center cursor-pointer">
-              <img src="/restros.svg" alt="" className="mr-2 max-sm:hidden" />
+              <Home className="mr-2 max-sm:hidden" />
               <div className="text-sm text-blue-400 max-sm:text-sm">
                 Home
               </div>
             </div>
-            <div className="bg-transparent h-14 rounded-full py-2 pl-4 px-5 w-fit flex items-center justify-center hover:border cursor-pointer">
-              <img src="/transport.svg" alt="" className="mr-2 max-sm:hidden" />
+            </a>
+            <a href="/chatbot">
+            <div className="bg-transparent h-14 rounded-full py-2 pl-4 px-5 w-fit flex items-center justify-center hover:border cursor-pointer text-white">
+              <Bot alt="" className="mr-2 max-sm:hidden" />
               <div className="text-sm text-white max-sm:text-sm">AI Chatbot</div>
             </div>
-            <div className="bg-transparent h-14 rounded-full py-2 pl-4 px-5 w-fit flex items-center justify-center hover:hover:border cursor-pointer">
-              <img src="/stays.svg" alt="" className="mr-2 max-sm:hidden" />
+            </a>
+            <a href="/dashboard">
+            <div className="bg-transparent h-14 rounded-full py-2 pl-4 px-5 w-fit flex items-center justify-center hover:hover:border cursor-pointer text-white">
+              <LayoutDashboard className="mr-2 max-sm:hidden" />
               <div className="text-sm text-white max-sm:text-sm">Dashboard</div>
             </div>
-            <div className="bg-transparent h-14 rounded-full py-2 pl-4 px-5 w-fit flex items-center justify-center hover:hover:border cursor-pointer">
-              <img src="/attracts.svg" alt="" className="mr-2 max-sm:hidden" />
+            </a>
+            <a href="/user-dashboard">
+            <div className="bg-transparent h-14 rounded-full py-2 pl-4 px-5 w-fit flex items-center justify-center hover:hover:border cursor-pointer text-white">
+              <Rocket className="mr-2 max-sm:hidden" />
               <div className="text-sm text-white max-sm:text-sm">
                 Automation
               </div>
             </div>
-          </div>
         </a>
+          </div>
         <div className="w-9/12 h-fit bg-white rounded-2xl shadow-2xl pl-4 pb-4">
           <div className="flex justify-around px-4 mt-10 max-md:flex-col ">
             <div>
